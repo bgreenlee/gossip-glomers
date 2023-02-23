049fab6856d72ee1ee835259ae96c6ab66acce11
Brad Greenlee <brad@footle.org>
2023-02-23 15:56:03 -0800

    Get it working again

--- maelstrom-broadcast/main.go
+++ maelstrom-broadcast/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"encoding/json"
 	"log"
-	"sync"
 	"time"
 
 	set "github.com/deckarep/golang-set/v2"
@@ -15,9 +14,6 @@ func main() {
 
 	messages := set.NewSet[int]() // messages we've received
 	var neighbors []string        // our neighbor nodes from the topology message
-
-	awaitingAcks := map[int][]string{} // map of message id -> nodes we're awaiting an ack from
-	var awaitingAcksMutex sync.RWMutex
 
 	n.Handle("broadcast", func(msg maelstrom.Message) error {
 		var broadcastMessage struct {
@@ -34,8 +30,8 @@ func main() {
 			"type": "broadcast_ok",
 		})
 
+		awaitingAcks := set.NewSet[string]()
 		message := broadcastMessage.Message
-		messageId := broadcastMessage.MsgId
 
 		// store and forward new messages
 		if !messages.Contains(message) {
@@ -44,50 +40,29 @@ func main() {
 			// forward message to our neighbors
 			for _, node := range neighbors {
 				if node != msg.Src { // don't send it to our source
-					awaitingAcksMutex.Lock()
-					awaitingAcks[messageId] = append(awaitingAcks[messageId], node)
-					awaitingAcksMutex.Unlock()
+					awaitingAcks.Add(node)
 				}
 			}
 
 			// send anything in awaitingAcks, and keep retrying until everything has acked
-			for len(awaitingAcks[messageId]) > 0 {
-				awaitingAcksMutex.RLock()
-				for _, node := range awaitingAcks[messageId] {
-					n.Send(node, msg.Body)
+			for awaitingAcks.Cardinality() > 0 {
+				for node := range awaitingAcks.Iter() {
+					go n.RPC(node, msg.Body, func(msg maelstrom.Message) error {
+						var broadcastOkMessage struct {
+							Type      string
+							InReplyTo int
+						}
+						if err := json.Unmarshal(msg.Body, &broadcastOkMessage); err != nil {
+							return err
+						}
+						awaitingAcks.Remove(node)
+						return nil
+					})
 				}
-				awaitingAcksMutex.RUnlock()
 				time.Sleep(1000 * time.Millisecond)
 			}
 		}
 
-		return nil
-	})
-
-	n.Handle("broadcast_ok", func(msg maelstrom.Message) error {
-		var broadcastOkMessage struct {
-			Type      string
-			InReplyTo int
-		}
-		if err := json.Unmarshal(msg.Body, &broadcastOkMessage); err != nil {
-			return err
-		}
-
-		// remove from awaitingAcks
-		awaitingAcksMutex.Lock()
-		defer awaitingAcksMutex.Unlock()
-		nodes := awaitingAcks[broadcastOkMessage.InReplyTo]
-		if len(nodes) <= 1 {
-			awaitingAcks[broadcastOkMessage.InReplyTo] = []string{}
-		} else {
-			// find our node
-			var i int
-			for i = 0; i < len(nodes) && nodes[i] != msg.Src; i++ {
-			}
-			// delete by replacing our node with the last one, and then truncating the slice by one
-			nodes[i] = nodes[len(nodes)-1]
-			awaitingAcks[broadcastOkMessage.InReplyTo] = nodes[:len(nodes)-1]
-		}
 		return nil
 	})
 
